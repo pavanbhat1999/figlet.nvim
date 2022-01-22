86e23e030fb52b2911905a580cc7eb82f7d3703a
pavanbhat1999 <prbhat07@gmail.com>
2022-01-22 22:39:00 +0530

    second commit

--- lua/firstPlugin/init.lua
+++ lua/firstPlugin/init.lua
@@ -10,17 +10,17 @@ local function Fig(arg1)
     vim.api.nvim_command(":read !figlet -f 3d "..arg1)
 end
 
-local function openWindow()
-	local bufh = vim.api.nvim_create_buf(true, false)
-	local winh = vim.api.nvim_open_win(bufh, true, {
-		relative = "editor",
-		width = 100,
-		height = 35,
-		row = 10,
-		col = 50,
-		border = "single",
-	})
-end
+-- local function openWindow()
+-- 	local bufh = vim.api.nvim_create_buf(true, false)
+-- 	local winh = vim.api.nvim_open_win(bufh, true, {
+-- 		relative = "editor",
+-- 		width = 100,
+-- 		height = 35,
+-- 		row = 10,
+-- 		col = 50,
+-- 		border = "single",
+-- 	})
+-- end
 local function newBuf()
 	local bufh = vim.api.nvim_create_buf(true, false)
 end
