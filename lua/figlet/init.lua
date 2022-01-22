e83903e66c56a9a87245e2e124bc488151e82e13
pavanbhat1999 <prbhat07@gmail.com>
2022-01-22 23:37:56 +0530

    changes not tested

--- lua/figlet/init.lua
+++ lua/figlet/init.lua
@@ -1,37 +1,24 @@
+--Global Settings-----------------------------------------------------------------------------------
+Font = "ANSI\\ Shadow"
+---------------------------------------------------------------------------------------------------
 local function openConfig()
 	local file = "$HOME/.config/nvim/init.lua"
 	vim.api.nvim_command("edit" .. file)
 end
-
+---------------------------------------------------------------------------------------------------
 local function openTerminal()
 	vim.api.nvim_command(":terminal")
 end
+---------------------------------------------------------------------------------------------------
 local function Fig(arg1)
-    vim.api.nvim_command(":read !figlet -f 3d "..arg1)
+	vim.api.nvim_command(":read !figlet -f "..Font.." ".. arg1)
 end
-
-local function openWindow()
-	local bufh = vim.api.nvim_create_buf(true, false)
-	local winh = vim.api.nvim_open_win(bufh, true, {
-		relative = "editor",
-		width = 100,
-		height = 35,
-		row = 10,
-		col = 50,
-		border = "single",
-	})
-end
-local function newBuf()
-	local bufh = vim.api.nvim_create_buf(true, false)
-end
-
+---------------------------------------------------------------------------------------------------
 return {
 
 	openConfig = openConfig,
 
 	openTerminal = openTerminal,
 
-	openWindow = openWindow,
-
-    Fig = Fig ,
+	Fig = Fig,
 }
--- plugin/figlet.nvim/figlet.vim
+++ plugin/figlet.nvim/figlet.vim
@@ -1,14 +1,13 @@
 fun! Figlet()
 	lua for k in pairs(package.loaded) do if k:match("^figlet") then package.loaded[k] = nil end end
-	lua require("figlet").openConfig()
+	"lua require("figlet").openConfig()
 endfun
 augroup Figlet
 autocmd!
 " -- Main Command-----------------------------------------------------------------------------------
 command! -nargs=1 Fig lua require'figlet'.Fig(<f-args>)
-
 " --------------------------------------------------------------------------------------------------
 command! OpenConfig lua require'figlet'.openConfig()
 command! OpenTerminal lua require'figlet'.openTerminal()
-command! OpenWindow lua require'figlet'.openWindow()
+" command! OpenWindow lua require'figlet'.openWindow()
 augroup END
