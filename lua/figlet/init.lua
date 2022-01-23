75709b13a9272933ab41c7c274605455950d3fec
pavanbhat1999 <prbhat07@gmail.com>
2022-01-23 11:52:41 +0530

    cleaning

--- lua/figlet/init.lua
+++ lua/figlet/init.lua
@@ -1,17 +1,16 @@
 --Global Settings-----------------------------------------------------------------------------------
 Font = "ANSI\\ Shadow"
+---------------------------------------------------------------------------------------------------
 local function Fig(arg1)
 	vim.api.nvim_command(":read !figlet -f " .. Font .. " " .. arg1)
 end
-
--- HACK: everything is hack here anyone can suggest a better way to do this
+-- HACK: everything is hack here anyone can suggest a better way to do this-------------------------
 local function FigComment(arg1)
 	vim.api.nvim_command(":read !figlet -f " .. Font .. " " .. arg1)
 	require("Comment.api").toggle_linewise_op("line")
 	vim.api.nvim_command("normal o")
 	vim.api.nvim_command("normal x")
 end
-
 return {
 	Fig = Fig,
 	FigComment = FigComment,
